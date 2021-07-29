a8f29adf9f26592d23c81e59ba0bd87387c1d1e2
sushi <sushi@rainbowism.com>
2021-07-29 02:16:56 -0400

    Fix concatenated packets crashing on parse

--- Scene.cs
+++ Scene.cs
@@ -6,6 +6,7 @@ public class Scene : Node2D {
 	const float TICK_RATE = 1.0f / 15;
 	// @Todo(sushi): make this load in from a file instead?
 	const string LOBBY_KEY = "lole";
+	const int PACKET_SIZE = sizeof(int) + 1 + (4 * sizeof(float));
 
 	[Export]
 	public PackedScene playerScene;
@@ -17,7 +18,7 @@ public class Scene : Node2D {
 	//  4 : player id int
 	//  1 : player flags
 	//  16: 4*float for pos/vel
-	byte[] sendBuffer = new byte[4 + sizeof(int) + 1 + (4 * sizeof(float))];
+	byte[] sendBuffer = new byte[4 + PACKET_SIZE];
 	UIntToByteLE id = new UIntToByteLE();
 	float[] movementBuffer = new float[4];
 	float accumulator;
@@ -93,35 +94,43 @@ public class Scene : Node2D {
 	}
 
 	public void Data() {
-		GD.Print("Data...");
-
-		// @Fix(sushi): handle concatenated packets. either use specified packet sizes to split,
-		//  or start using packet ids so we can expand the demo later with special interactions
+		// @Todo(sushi): start using a packet processor so we can expand the demo later with special interactions
 		byte[] data = peer.GetPacket();
-		if (data.Length == 0 || data.Length % 21 != 0) {
+		if (data.Length == 0 || data.Length % PACKET_SIZE != 0) {
 			return;
 		}
 
-		id.B0 = data[0];
-		id.B1 = data[1];
-		id.B2 = data[2];
-		id.B3 = data[3];
+		for (int offset = 0; offset < data.Length; offset += PACKET_SIZE) {
+			id.B0 = data[offset + 0];
+			id.B1 = data[offset + 1];
+			id.B2 = data[offset + 2];
+			id.B3 = data[offset + 3];
 
-		// copy the movement floats into the buffer, ignoring the id/state bytes
-		Buffer.BlockCopy(data, 5, movementBuffer, 0, data.Length - 5);
+			// copy the movement floats into the buffer, ignoring the id/state bytes
+			Buffer.BlockCopy(data, offset + 5, movementBuffer, 0, movementBuffer.Length * 4);
 
-		Player player;
-		if (!players.ContainsKey(id.Value)) {
-			player = playerScene.Instance() as Player;
-			players[id.Value] = player;
-			AddChild(player);
-		} else {
-			player = players[id.Value];
+			Player player;
+			if (!players.ContainsKey(id.Value)) {
+				player = playerScene.Instance() as Player;
+				players[id.Value] = player;
+				AddChild(player);
+			} else {
+				player = players[id.Value];
+			}
+			player.RemoteState = data[offset + 4];
+			player.timeSinceLastUpdate = 0.0f;
+
+			// player position smoothing so it doesn't instantly snap
+			Vector2 remotePosition = new Vector2(movementBuffer[0], movementBuffer[1]);
+			Vector2 difference = remotePosition - player.Position;
+			float distance = difference.Length();
+			if (distance > 30.0f) {
+				player.Position = remotePosition;
+			} else if (distance > 2.0f) {
+				player.Position += difference * 0.1f;
+			}
+			player.velocity = new Vector2(movementBuffer[2], movementBuffer[3]);
 		}
-		player.RemoteState = data[4];
-		player.timeSinceLastUpdate = 0.0f;
-		player.Position = new Vector2(movementBuffer[0], movementBuffer[1]);
-		player.velocity = new Vector2(movementBuffer[2], movementBuffer[3]);
 	}
 
 	public void CloseRequest(int code, String reason) {
